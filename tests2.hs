2bd67a82761e6947ad833e29cc9d6f8f9c5e71bb
Beren Millidge <bmillidgework@gmail.com>
2018-04-25 15:03:13 +0100

    Add files via upload

--- tests2.hs
+++ tests2.hs
@@ -12,7 +12,7 @@ factorial n = n * factorial (n-1)
 
 addVectors :: (Num a) => (a,a) -> (a,a) -> (a,a)
 addVectors (x1, y1) (x2,y2) = (x1+x2, y1+y2)
-addVectors a b = (fst a + fst b, snd a + snd b)
+--addVectors a b = (fst a + fst b, snd a + snd b)
 
 head' :: [a] -> a
 head' [] = error "Can't call head on an empty list"
@@ -27,3 +27,48 @@ length' (_:xs) = 1 + length' xs
 sum' :: (Num a) => [a] -> a
 sum' [] = 0
 sum' (x:xs) = x + sum' xs
+
+bmiTell :: (RealFloat a) => a -> String  
+bmiTell bmi  
+    | bmi <= 18.5 = "You're underweight, you emo, you!"  
+    | bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"  
+    | bmi <= 30.0 = "You're fat! Lose some weight, fatty!"  
+    | otherwise   = "You're a whale, congratulations!" 
+
+bmiTell :: (RealFloat a) => a -> a -> String  
+bmiTell weight height  
+    | bmi  <= 18.5 = "You're underweight, you emo, you!"  
+    | bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"  
+    | bmi <= 30.0 = "You're fat! Lose some weight, fatty!"  
+    | otherwise  = "Normal!"
+    where bmi = weight/height ^ 2       
+
+max' :: (Ord a) => a -> a -> a
+max' a b
+     | a> b = a
+     | otherwise = b
+
+myCompare:: (Ord a) => a -> a -> Ordering
+myCompare a b
+| a > b = GT
+| a == b = EQ
+| otherwise = LT
+
+initials:: String -> String -> String
+initial firstname lastname = [f] ++ "." ++ [l] ++ "."
+        where (f:_) = firstname
+              (l:_) = lastname
+
+cyclinder:: (RealFloat a) => a -> a -> a
+cyclinder r h = 
+	let sideArea = 2 * pi * r * h
+		topArea = pi* r^2
+	in sideArea + 2 * topArea
+
+calcBmis:: (RealFloat a) =. [(a,a)] -> [a]
+calcBmis xs = [bmi | (w,h) <- xs, let bmi = w/h^2, bmi>25.0]
+
+describeList:: [a] -> String
+describeList xs = "The list is " ++ case xs of [] -> "empty."
+												[x] -> "A singleton list"
+												xs -> "a longer list"
